a40bb1899508dcd5312037e8f1a2978cfbee489c
Simon Steindl <kosimst@gmail.com>
2018-08-24 13:07:24 +0200

    Optimized form.js

--- js/form.js
+++ js/form.js
@@ -1,79 +1,116 @@
-var eventTypes = {
-	newRant:          [
-		{
-			id:          'byUser',
-			name:        'By User',
-			placeholder: 'User(s) ...',
-			help:        '(Optional) Execute only when specific user(s) posts a rant. Comma-separate for multiple',
-			icon:        'user'
-		},
-		{
-			id:          'withTag',
-			name:        'With Tag',
-			placeholder: 'Tag(s) ...',
-			help:        '(Optional) Execute only when rant has specific tag(s). Comma-separate for multiple',
-			icon:        'tag'
-		}
-	],
-	newCommentOnRant: [
-		{
-			id:          'rantID',
-			name:        'Rant ID',
-			placeholder: 'Rant ID ...',
-			help:        'The rant ID. (You can find it in the URL)',
-			icon:        'hashtag'
-		},
-		{
-			id:          'byUser',
-			name:        'By User',
-			placeholder: 'User(s) ...',
-			help:        '(Optional) Execute only when specific user(s) posts a comment. Comma-separate for multiple',
-			icon:        'user'
-		}
-	],
-	newWeeklyTopic:   []
-};
+// Declaration with let to prevent global
+let eventTypes = {
+  newRant: [
+    {
+      id: 'byUser',
+      name: 'By User',
+      placeholder: 'User(s) ...',
+      help:
+        '(Optional) Execute only when specific user(s) posts a rant. Comma-separate for multiple',
+      icon: 'user',
+    },
+    {
+      id: 'withTag',
+      name: 'With Tag',
+      placeholder: 'Tag(s) ...',
+      help:
+        '(Optional) Execute only when rant has specific tag(s). Comma-separate for multiple',
+      icon: 'tag',
+    },
+  ],
+  newCommentOnRant: [
+    {
+      id: 'rantID',
+      name: 'Rant ID',
+      placeholder: 'Rant ID ...',
+      help: 'The rant ID. (You can find it in the URL)',
+      icon: 'hashtag',
+    },
+    {
+      id: 'byUser',
+      name: 'By User',
+      placeholder: 'User(s) ...',
+      help:
+        '(Optional) Execute only when specific user(s) posts a comment. Comma-separate for multiple',
+      icon: 'user',
+    },
+  ],
+  newWeeklyTopic: [],
+}
 
-$(document).ready(function () {
-	if ('eventType' in previousForm && previousForm.eventType != '') $('#eventType').val(previousForm.eventType);
-	if ('method' in previousForm && previousForm.method != '') $('#method').val(previousForm.method);
-	if ('contentType' in previousForm && previousForm.contentType != '') $('#contentType').val(previousForm.contentType);
-	$('#eventType').trigger('change');
-});
+const eventType = document.getElementById('eventType')
 
-$('#eventType').on('change', function (event) {
-	var eventType = event.currentTarget.value;
+// Vanilla implementation of $(document).ready
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict'
 
-	$('#additionalFields').empty();
+  // Use getElementById -> faster and vanilla
+  if ('eventType' in previousForm && previousForm.eventType != '') {
+    eventType.value = previousForm.eventType
+  }
+  if ('method' in previousForm && previousForm.method != '') {
+    document.getElementById('method').value = previousForm.method
+  }
+  if ('contentType' in previousForm && previousForm.contentType != '') {
+    document.getElementById('contentType').value = previousForm.contentType
+  }
+  // Vanilly implementaion of $().trigger
+  eventType.dispatchEvent(new Event('change'))
+})
 
-	if (Object.keys(eventTypes).indexOf(eventType) > -1) {
-		for (var i = 0; i < eventTypes[eventType].length; i++) {
-			var data = eventTypes[eventType][i];
+// Use native event listeners
+eventType.addEventListener('change', event => {
+  'use strict'
 
-			data.error = false;
-			if (data.id in errors) {
-				data.help = errors[data.id];
-				data.error = true;
-			}
+  // eventTypoe is already declared
+  // var eventType = event.currentTarget.value
 
-			var newField = '<div class="field is-horizontal">' +
-			               '  <div class="field-label is-normal">' +
-			               '    <label class="label has-text-white">' + data.name + '</label>' +
-			               '  </div>' +
-			               '  <div class="field-body">' +
-			               '    <div class="field"> ' +
-			               '      <div class="control has-icons-left">' +
-			               '        <input value="' + (data.id in previousForm ? previousForm[data.id] : '') + '" id="' + data.id + '" name="' + data.id + '" class="input ' + (data.error ? 'is-danger' : '') + '" type="text" placeholder="' + data.placeholder + '">' +
-			               '        <span class="icon is-small is-left has-text-white">' +
-			               '          <i class="fas fa-' + data.icon + '"></i>' +
-			               '        </span>' +
-			               '      </div>' +
-			               '      <p class="help ' + (data.error ? 'is-danger' : '') + '">' + data.help + '</p>' +
-			               '    </div>' +
-			               '  </div>' +
-			               '</div>';
+  // Vanilla $().empty
+  const additionalFields = document.getElementById('additionalFields')
+  additionalFields.innerHTML = ''
 
-			$('#additionalFields').append(newField);
-		}
-	}
-});
+  // Use Array.prototype.includes
+  if (Object.keys(eventTypes).includes(eventType)) {
+    // Important: Always use let in for loops -> scoped!
+    // TODO: Convert to for…of loop -> faster
+    for (let i = 0; i < eventTypes[eventType].length; i++) {
+      const data = eventTypes[eventType][i]
+
+      data.error = false
+      if (data.id in errors) {
+        data.help = errors[data.id]
+        data.error = true
+      }
+
+      // Use Template-String-Literal for better readabilty
+
+      const newField = `
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label has-text-white">${data.name}</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control has-icons-left">
+              <input
+                value="${data.id in previousForm ? previousForm[data.id] : ''}"
+                id="${data.id}"
+                name="${data.id}"
+                class="input ${data.error ? 'is-danger' : ''}"
+                type="text"
+                placeholder="${data.placeholder}"
+              >
+              <span class="icon is-small is-left has-text-white">
+                <i class="fas fa-${data - icon}></i>
+              </span>
+            </div>
+            <p class="help ${data.error ? 'is-danger' : ''}">${data.help}</p>
+          </div>
+        </div>
+      </div>
+      `
+
+      additionalFields.insertAdjacentHTML('beforeend', newField)
+    }
+  }
+})
